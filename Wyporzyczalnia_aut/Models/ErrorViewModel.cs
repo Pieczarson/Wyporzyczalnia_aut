cec32a99e2e6d9d280c38a027e5117cf31d2f5fd
pc <pc@DESKTOP-8GUSCCJ>
2024-12-15 10:30:10 +0100

    Dodaj pliki projektów.

---
+++ Wyporzyczalnia_aut/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wyporzyczalnia_aut.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
